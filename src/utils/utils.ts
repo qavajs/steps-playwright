c2e4a7e80529f4a0d068f262c21605fe111f7f31
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2023-01-09 18:29:46 +0300

    added scroll by offset steps

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -29,3 +29,12 @@ export function traceArchive(driverConfig: any, scenario: ITestCaseHookParameter
         `${normalizeScenarioName(scenario.pickle.name)}-${scenario.testCaseStartedId}.zip`
     )
 }
+
+/**
+ * Parse 'x, y' string to coordinates array
+ * @param {string} coords - 'x, y' string
+ * @return {number[]} - coords array
+ */
+export function parseCoords(coords: string): number[] {
+    return coords.split(/\s?,\s?/).map((c: string) => parseFloat(c ?? 0))
+}
