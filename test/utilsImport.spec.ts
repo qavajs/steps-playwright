39a87479276c86e4b2dea52f50c7cd7f4e5d8b79
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2023-05-26 10:58:43 +0300

    moved test to vitest

--- test/utilsImport.spec.ts
+++ test/utilsImport.spec.ts
@@ -1,10 +1,10 @@
 import { getValue, getValueWait, getConditionWait, getElement } from '../utils.js'
-import { test, expect } from '@jest/globals';
+import { test, expect } from 'vitest';
 test.each([
     getValue,
     getValueWait,
     getConditionWait,
     getElement
-])('util function %p', (fn) => {
+])('util function %o', (fn) => {
     expect(fn).toBeInstanceOf(Function);
 });
