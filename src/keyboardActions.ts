6dfaf454c2de0a195186116f0916ec9f9f8fc4fc
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2023-06-01 14:17:54 +0300

    added docs for keyboard and mouse keys

--- src/keyboardActions.ts
+++ src/keyboardActions.ts
@@ -1,11 +1,19 @@
 import { When } from '@cucumber/cucumber';
-import { getValue } from './transformers';
-import { parseCoords } from './utils/utils';
 
+/**
+ * Press and hold keyboard key
+ * @param {string} key - key to press
+ * @example When I hold down 'Q' key
+ */
 When('I hold down {string} key', async function (key) {
     await page.keyboard.down(key);
 });
 
+/**
+ * Release keyboard key
+ * @param {string} key - key to release
+ * @example When I release 'A' key
+ */
 When('I release {string} key', async function (key) {
     await page.keyboard.up(key);
 });
