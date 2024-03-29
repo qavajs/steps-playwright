b2b61f5e412c0c01937337e3f4ce82ecd7953103
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2024-03-29 20:44:41 +0200

    fixed vitest path

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    include: [
+        "test/*.ts"
+    ],
     coverage: {
       provider: 'v8',
       include: ["src/browserManager.ts"],
